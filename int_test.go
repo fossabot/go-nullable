bb768ab0e526ca108f97bac390789cb1e0ffa5a5
hassieswift621 <hassie.dev@gmail.com>
2019-05-21 14:12:30 +0100

    Add more tests

--- int_test.go
+++ int_test.go
@@ -1,7 +1,10 @@
 package nullable
 
 import (
+	"bytes"
 	"encoding/json"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -14,7 +17,19 @@ func TestInt_IsValid(t *testing.T) {
 }
 
 func TestInt_MarshalJSON(t *testing.T) {
+	v := 83794
 
+	// Encode into JSON.
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(IntFrom(v)); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expected := strconv.FormatInt(int64(v), 10)
+	actual := strings.TrimSpace(buf.String())
+	if actual != expected {
+		t.Errorf("Int was invalid, expected %s, got %s", expected, actual)
+	}
 }
 
 func TestInt_Value(t *testing.T) {
@@ -41,12 +56,12 @@ func TestInt_UnmarshalJSON(t *testing.T) {
 	type St struct {
 		Val Int `json:"val"`
 	}
-	var parsed St
-	if err := json.Unmarshal(data, &parsed); err != nil {
+	var decoded St
+	if err := json.Unmarshal(data, &decoded); err != nil {
 		t.Errorf(err.Error())
 	}
 
-	actual := parsed.Val.Value()
+	actual := decoded.Val.Value()
 	if actual != 7386 {
 		t.Errorf("Int was not valid, expected %d, got %d", 7386, actual)
 	}
